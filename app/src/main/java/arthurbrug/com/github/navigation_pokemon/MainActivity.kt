a631eb575a8d474f5756c4e0751ae01cbaa5040f
Arthur <arthurbrugnerottocerqueira@gmail.com>
2026-03-17 20:29:25 -0300

    Removendo exemplos e limpando o MainActivity para futuras implementações

---
+++ app/src/main/java/arthurbrug/com/github/navigation_pokemon/MainActivity.kt
@@ -0,0 +1,29 @@
+package arthurbrug.com.github.navigation_pokemon
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import arthurbrug.com.github.navigation_pokemon.ui.theme.NavigationpokemonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationpokemonTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+
+                }
+            }
+        }
+    }
+}
