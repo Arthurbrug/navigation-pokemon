155cebd73881a43f3728dd030f7001e49334dd98
Arthur <arthurbrugnerottocerqueira@gmail.com>
2026-03-17 21:30:26 -0300

    Criação da tela de Login (LoginScreen) com layout inicial e botão para escolher seu pokemon

---
+++ app/src/main/java/arthurbrug/com/github/navigation_pokemon/screens/EscolhaPokemonScreen.kt
@@ -0,0 +1,93 @@
+package arthurbrug.com.github.navigation_pokemon.screens
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun EscolhaPokemonScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFF2C4EC7))
+            .padding(32.dp)
+    ) {
+        Text(
+            text = "Escolha seu pokemon inicial:",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Center)
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(Color.White),
+                modifier = Modifier.size(width = 200.dp, height = 48.dp)
+            ) {
+                Text(
+                    text = "Bulbasaur",
+                    fontSize = 20.sp,
+                    color = Color.Green
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(Color.White),
+                modifier = Modifier.size(width = 200.dp, height = 48.dp)
+            ) {
+                Text(
+                    text = "Charmander",
+                    fontSize = 20.sp,
+                    color = Color.Red
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(Color.White),
+                modifier = Modifier.size(width = 200.dp, height = 48.dp)
+            ) {
+                Text(
+                    text = "Squirtle",
+                    fontSize = 20.sp,
+                    color = Color.Blue
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(Color.White),
+                modifier = Modifier.size(width = 200.dp, height = 48.dp)
+            ) {
+                Text(
+                    text = "Sair, não escolho nenhum",
+                    fontSize = 20.sp,
+                    color = Color.Blue
+                )
+            }
+        }
+    }
+}
